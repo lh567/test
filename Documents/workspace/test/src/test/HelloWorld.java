4799f81d5d8d2aaa5b8f9e70ca10f4a6cb3acc23
Liwei Han <lh567@cornell.edu>
2015-09-04 13:12:40 -0400

    test2

--- Documents/workspace/test/src/test/HelloWorld.java
+++ Documents/workspace/test/src/test/HelloWorld.java
@@ -2,6 +2,6 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		for(int i = 0; i < 10; i++)	System.out.println("Hello World!");
 	}
 }
